f505141ff81258e52486eb27ab821807f9f7f064
kpendurthi <pendurthikirank@johndeere.com>
2020-03-18 10:56:45 -0500

    commiting index.js changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const usersController = require('./controllers/users');
 
 //middleware
 app.use(express.json());
+app.use(cors())
 
 //controllers
 app.use('/api/users',usersController);
